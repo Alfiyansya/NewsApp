4a8cdc1042026ecfaf2e23a06e984972ce512e39
Achmad Alfiansyah <achmadalfiansyah069@gmail.com>
2022-11-18 13:55:47 +0700

    setting up a retrofit

---
+++ app/src/main/java/com/alfiansyah/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.alfiansyah.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "20bb2a0cbf574d8cb37f949736d41dab"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
